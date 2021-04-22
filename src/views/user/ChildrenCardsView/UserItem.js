496eac50dc1c1bf77abf93684cb34c73915f52b9
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-04-22 21:50:01 +0200

    Add CardActionsArea and Link component to Child Card

--- src/views/user/ChildrenCardsView/UserItem.js
+++ src/views/user/ChildrenCardsView/UserItem.js
@@ -1,27 +1,16 @@
 import clsx from 'clsx';
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Icon } from '@iconify/react';
-import { fShortenNumber } from '~/utils/formatNumber';
+import {Icon} from '@iconify/react';
 import twitterFill from '@iconify-icons/eva/twitter-fill';
 import linkedinFill from '@iconify-icons/eva/linkedin-fill';
 import facebookFill from '@iconify-icons/eva/facebook-fill';
 import instagramFilled from '@iconify-icons/ant-design/instagram-filled';
-import { getImgCover, getImgFeed, getImgAvatar } from '~/utils/getImages';
-import { alpha, makeStyles } from '@material-ui/core/styles';
-import {
-  Box,
-  Card,
-  Grid,
-  Avatar,
-  Tooltip,
-  Divider,
-  CardMedia,
-  Typography,
-  IconButton,
-  CardContent
-} from '@material-ui/core';
-import { MIcon } from '~/@material-extend';
+import {getImgAvatar, getImgCover} from '~/utils/getImages';
+import {alpha, makeStyles} from '@material-ui/core/styles';
+import {Avatar, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography} from '@material-ui/core';
+import {MIcon} from '~/@material-extend';
 
 // ----------------------------------------------------------------------
 
@@ -101,47 +90,52 @@ function UserItem({ user, className }) {
 
   let index = getRandomInt(1, 24);
 
-  return (
-    <Card className={clsx(classes.root, className)}>
-      <div className={classes.cardMediaWrap}>
-        <MIcon
-          size={144}
-          color="paper"
-          src="/static/icons/shape-avatar.svg"
-          className={classes.avatarShape}
-        />
-        <Avatar
-          alt={name}
-          src={getImgAvatar(index)}
-          sx={{
-            width: 64,
-            height: 64,
-            zIndex: 11,
-            position: 'absolute',
-            transform: 'translateY(-50%)'
-          }}
-        />
-        <CardMedia
-          component="img"
-          title="cover"
-          data-sizes="auto"
-          src="/static/images/placeholder.svg"
-          data-src={getImgCover(600, index)}
-          data-srcset={`${getImgCover(600, index)} 600w, ${getImgCover(
-            960,
-            index
-          )} 960w`}
-          className={clsx(classes.cardMedia, 'lazyload blur-up')}
-        />
-      </div>
+    return (
+      <CardActionArea
+        component={Link} to='/'>
+        <Card className={clsx(classes.root, className)}>
 
-      <CardContent className={classes.cardContent}>
-        <Typography variant="subtitle1" align="center">
-          {name}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+
+          <div className={classes.cardMediaWrap}>
+            <MIcon
+              size={144}
+              color="paper"
+              src="/static/icons/shape-avatar.svg"
+              className={classes.avatarShape}
+            />
+            <Avatar
+              alt={name}
+              src={getImgAvatar(index)}
+              sx={{
+                width: 64,
+                height: 64,
+                zIndex: 11,
+                position: 'absolute',
+                transform: 'translateY(-50%)'
+              }}
+            />
+            <CardMedia
+              component="img"
+              title="cover"
+              data-sizes="auto"
+              src="/static/images/placeholder.svg"
+              data-src={getImgCover(600, index)}
+              data-srcset={`${getImgCover(600, index)} 600w, ${getImgCover(
+                960,
+                index
+              )} 960w`}
+              className={clsx(classes.cardMedia, 'lazyload blur-up')}
+            />
+          </div>
+
+          <CardContent className={classes.cardContent}>
+            <Typography variant="subtitle1" align="center">
+              {name}
+            </Typography>
+          </CardContent>
+      </Card>
+      </CardActionArea>
+    );
 }
 
 export default UserItem;
