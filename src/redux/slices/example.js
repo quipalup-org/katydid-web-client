cf7e01d0acd9979bdb6cbd28b2ad6783afa929ad
ashikka <ashikagupta28@gmail.com>
2021-07-06 14:36:45 +0530

    fix: response

--- src/redux/slices/example.js
+++ src/redux/slices/example.js
@@ -5,18 +5,28 @@ export default function exampleResponse() {
         id: '550e8400-e29b-41d4-a716-446655440000',
         type: 'meal',
         attributes: {
-          time: 123345534,
+          childId: 'sample-child-id-1',
+          time: '12:54',
           description: 'Yogurt with strawberries',
           amount: 100,
           unit: 'percentage'
+        },
+        links: {
+          iconURL:
+            'https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/meal-icon.svg'
         }
       },
       {
         id: '550e8400-e29b-41d4-a716-446655441234',
         type: 'nap',
         attributes: {
-          time: 1234,
-          duration: 2472243
+          childId: 'sample-child-id-1',
+          time: '18:39',
+          duration: 59
+        },
+        links: {
+          iconURL:
+            'https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/nap-icon.svg'
         }
       }
     ]
