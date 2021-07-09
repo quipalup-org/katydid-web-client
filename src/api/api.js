5acb63e4510cc4dc7af7d6f0d476386820a8bacd
ashikka <ashikagupta28@gmail.com>
2021-07-10 01:01:04 +0530

    feat: nested params in axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
+import Qs from 'qs';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
   responseType: 'json'
+});
+
+api.interceptors.request.use(config => {
+  config.paramsSerializer = params => {
+    return Qs.stringify(params, {
+      arrayFormat: 'brackets',
+      encode: true
+    });
+  };
+
+  return config;
 });
 
 export default api;
