4a93e2e510856f627ecfd8ff8c5eb7e1727619dd
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-07-11 12:32:52 +0200

    Fix convertoTimestampToHrMin()

--- src/utils/convertTimeStampToHrMin.js
+++ src/utils/convertTimeStampToHrMin.js
@@ -1,5 +1,5 @@
 const convertTimeStampToHrMin = timestamp => {
-  const date = new Date(timestamp);
+  const date = new Date(timestamp * 1000);
   const hour = ('0' + date.getHours()).slice(-2);
   const mins = ('0' + date.getMinutes()).slice(-2);
   return `${hour}:${mins}`;
